0b5be8afe40ce15aa404e8ae5c3cb3030c67e5de
leejiseong <sage@i-on.net>
2023-08-12 18:40:57 +0900

    기존테스트에 시큐리티 적용

--- src/test/java/com/aws/awsspringboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/aws/awsspringboot/web/dto/HelloResponseDtoTest.java
@@ -17,7 +17,7 @@ public class HelloResponseDtoTest {
         HelloResponseDto dto = new HelloResponseDto(name, amount);
 
         //then
-        assertThat(dto.getName()).isEqualTo("name");
+        assertThat(dto.getName()).isEqualTo(name);
         assertThat(dto.getAmount()).isEqualTo(amount);
     }
 
